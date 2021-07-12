0441eef84a34d1cf375ee389452c3b38c2559ae9
SameerInstacash <sameer.khan@getinstacash.in>
2021-07-12 13:41:17 +0530

    Initial Commit

---
+++ TechCheck ExchangeTests/TechCheck_ExchangeTests.swift
@@ -0,0 +1,33 @@
+//
+//  TechCheck_ExchangeTests.swift
+//  TechCheck ExchangeTests
+//
+//  Created by Sameer Khan on 12/07/21.
+//
+
+import XCTest
+@testable import TechCheck_Exchange
+
+class TechCheck_ExchangeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
